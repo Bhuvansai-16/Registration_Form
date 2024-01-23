7143fbd621d0f85280245fd831232f7f608ad014
Bhuvansai-16 <chbhuvansai522@gmail.com>
2024-01-23 20:00:41 +0530

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var db=mongoose.connection
 db.on('error',()=> console.log("Error in Connecting to Database"))
 db.once('open',()=> console.log("Connected to Database"))
 
-app.post("/sign_up",(req,res) => {
+app.post("signup_successful.html",(req,res) => {
     var name= req.body.name
     var age=req.body.age
     var email=req.body.email
@@ -31,7 +31,7 @@ app.post("/sign_up",(req,res) => {
         "gender":gender,
         "password":password
     }
-    db.collection('users').insertOne(data,(err,collection) => {
+    db.collection('db1').insertOne(data,(err,collection) => {
         if(err){
             throw err;
         }
